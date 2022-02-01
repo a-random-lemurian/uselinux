0dccf5148553a0a56b3b845c16d2ab28a784e826
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-01 22:42:43 +0800

    docs(ancientpkg): Add doxygen to functions

--- src/ancientpkg.h
+++ src/ancientpkg.h
@@ -2,7 +2,19 @@
 #ifndef ANCIENTPKG_H
 #define ANCIENTPKG_H
 
+/**
+ * @brief Dig up ancient Debian packages.
+ *
+ */
 void ancient_debian_packages();
-int check_digsite();
+
+/**
+ * @brief Look for ancient Debian packages at a digsite.
+ * 
+ * @param i Digsite number.
+ * 
+ * @return int, Amount of Debian packages found.
+ */
+int check_digsite(int i);
 
 #endif
