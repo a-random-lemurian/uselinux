70e54de0068e62b0fa8f23a664cd198b6af43f21
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-03 00:15:15 +0800

    feat(lib): Add opener lib for URL opening

---
+++ opener/detect_os.h
@@ -0,0 +1,24 @@
+// Modified by Lemuria (renamed to prevent name conflicts)
+
+
+#ifndef DETECT_OS_H
+#define DETECT_OS_H
+
+//
+// os.h
+//
+// MIT licensed.
+// Copyright (c) Abraham Hernandez <abraham@abranhe.com>
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char * operating_system(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OS_H
