757bf924b9a54120af2535637a74ec86351b2eba
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-05 09:20:17 +0800

    style(penguinspam): Reformat code

--- src/penguinspam/penguinspam.c
+++ src/penguinspam/penguinspam.c
@@ -10,36 +10,33 @@
 
 #include "penguinspam.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-void free_psj_mem(penguin_spam_job *job)
-{
-    free(job->__error__);
-}
+void free_psj_mem(penguin_spam_job *job) { free(job->__error__); }
 
 int mk_penguin_spam_job(penguin_spam_job *job, int penguins, char *location)
 {
     job->__error__ = malloc(512);
 
-
     if (!strcmp(location, ""))
     {
-        sprintf(job->__error__,"Error: Location cannot be an empty string");
+        sprintf(job->__error__, "Error: Location cannot be an empty string");
         return PENGUIN_SPAM_ERROR;
     }
     if (penguins > MAX_PENGUINS)
     {
-        sprintf(job->__error__,"Error: Too many penguins (limit is %d, "
-                 "tried to send %d)",
-                  MAX_PENGUINS, penguins);
+        sprintf(job->__error__,
+                "Error: Too many penguins (limit is %d, "
+                "tried to send %d)",
+                MAX_PENGUINS, penguins);
         return PENGUIN_SPAM_TOO_MANY_PENGUINS;
     }
     if (penguins <= 0)
     {
         sprintf(job->__error__,
-            "Error: penguins must be a positive number (tried to send %d)",
-             penguins);
+                "Error: penguins must be a positive number (tried to send %d)",
+                penguins);
         return PENGUIN_SPAM_ERROR;
     }
 
@@ -56,7 +53,7 @@ int execute_penguin_spam_job(penguin_spam_job *job)
     printf("Successfully spammed penguins.\n");
 
     return PENGUIN_SPAM_OK;
-};
+}
 
 int finish_penguin_spam_job(penguin_spam_job *job)
 {
--- src/penguinspam/penguinspam.h
+++ src/penguinspam/penguinspam.h
@@ -3,20 +3,16 @@
  * @author Lemuria
  * @brief Header file for penguin spam.
  * @date 2022-02-04
- * 
+ *
  * @copyright MIT License
- * 
+ *
  */
-
 
 #ifndef PENGUINSPAM_H
 #define PENGUINSPAM_H
 
 /** @brief Limit on how many penguins can be sent. */
 #define MAX_PENGUINS 5000000
-
-
-
 
 /** @brief Operation was successful.*/
 #define PENGUIN_SPAM_OK 0
@@ -25,31 +21,32 @@
 /** @brief An attempt was made to send too many penguins.*/
 #define PENGUIN_SPAM_TOO_MANY_PENGUINS 2
 
-typedef struct penguin_spam_job {
-    char* location;
+typedef struct penguin_spam_job
+{
+    char *location;
     int penguins;
 
-    char* __error__;
+    char *__error__;
 } penguin_spam_job;
 
 /**
  * @brief Prepare a penguin spam job.
- * 
+ *
  * @param job Pointer to a penguin_spam_job struct.
  * @param penguins Number of penguins to send.
  * @param location Location where penguins will be sent.
  * @return int Status code of operation.
  */
-int mk_penguin_spam_job(penguin_spam_job* job, int penguins, char* location)
+int mk_penguin_spam_job(penguin_spam_job *job, int penguins, char *location)
     __attribute__((warn_unused_result));
 
 /**
  * @brief Execute a penguin spam job.
- * 
+ *
  * @param job Pointer to a penguin_spam_job struct.
  * @return int Status code of operation.
  */
-int execute_penguin_spam_job(penguin_spam_job* job);
+int execute_penguin_spam_job(penguin_spam_job *job);
 
 /**
  * @brief Finish a penguin spam job.
