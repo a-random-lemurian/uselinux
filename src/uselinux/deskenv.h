b350f49a9fb8f22ddf3702cc904fe76385a5ab06
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-06 11:59:07 +0800

    docs: Document desktop_environments() function

--- src/uselinux/deskenv.h
+++ src/uselinux/deskenv.h
@@ -1,6 +1,11 @@
 #ifndef DESKENV_H
 #define DESKENV_H
 
+/**
+ * @brief React to a desktop environment supplied by the user.
+ *
+ * @param desk Name of desktop environment. Case insensitive.
+ */
 void desktop_environments(char* desk);
 
 #endif /* DESKENV_H */
