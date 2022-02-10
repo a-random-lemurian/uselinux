a5ce4869c9283738253bec47988b1166d5bb7b0c
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-10 08:58:00 +0800

    refactor(rmbloat-rm): use argparse instead

--- src/rmbloat/rmbloat-rm.c
+++ src/rmbloat/rmbloat-rm.c
@@ -1,51 +1,39 @@
-#include <argp.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <argparse.h>
 #include "rmbloat-common.h"
 #include "rmbloatapi.h"
 
 
 static int fast_flag = 0;
 
-void print_help()
-{
-    printf(
-        "rmbloat-rm - remove bloatware\n\n"
 
-        "    -h, --help           Program help\n"
-        "    -l, --list           List of software to remove\n"
-        "        --fast           Be faster\n"
-    );
-    printf("\n");
-}
+static const char *const usage[] = {
+    "rmbloat-rm [PACKAGES]",
+    "rmbloat-rm --help",
+    "rmbloat-rm --fast [PACKAGES]\n",
+    NULL
+};
 
 
 int main(int argc, char** argv)
 {
-    for (int i = 1; i < argc; i++)
+    int fast = 0;
+
+    struct argparse argparse;
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_BOOLEAN(0, "fast", &fast, "Remove bloat faster"),
+        OPT_END()
+    };
+
+    argparse_init(&argparse, options, usage, 0);
+    argparse_describe(&argparse, "Remove bloat from the command line","\n");
+    argc = argparse_parse(&argparse, argc, (const char**)argv);
+
+    for (int i = 0; i < argc; i++)
     {
-        if (!strncmp(argv[i], "--help", 7) || !strncmp(argv[i], "-h", 2))
-        {
-            print_help();
-            exit(0);
-        }
-        else if (!strncmp(argv[i], "--list", 7) || !strncmp(argv[i], "-l", 2))
-        {
-            i++;
-            for (int idx = i; idx < argc; idx++)
-            {
-                remove_bloatware(argv[idx], fast_flag);
-            }
-            exit(0);
-        }
-        else if (!strncmp(argv[i], "--fast", 7))
-        {
-            fast_flag = 1;
-        }
-        else
-        {
-            handle_bad_arg(argv[i]);
-        }
+        remove_bloatware(argv[i], fast);
     }
 }
