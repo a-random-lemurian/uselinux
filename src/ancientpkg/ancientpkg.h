7bd478f0d6f925ccb6c9c442503fe936308f16e2
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-13 12:07:39 +0800

    feat(ancientpkg): add small package system

--- src/ancientpkg/ancientpkg.h
+++ src/ancientpkg/ancientpkg.h
@@ -16,6 +16,13 @@ typedef struct PackageDatingResult {
     char* dating_method;
 } PackageDatingResult;
 
+typedef struct Package {
+    char* name;
+    char* maintainer;
+    char** maintainers_ls;
+    char* license;
+} Package;
+
 int cmd_dig(int argc, char** argv);
 int cmd_date(int argc, char** argv);
 int dig_from_json(char* filename);
@@ -24,4 +31,10 @@ int has_missing_args(char* location, int archaeologists,
 int dig_common(int archaeologists, int expected_packages,
                int verbose, int passes, char* location);
 void package_shard_failure(int i, char* pkgname);
+
+/* Packages ******************************************************************/
+int validate_package(Package* self);
+int create_package(Package* self, char* name, char* license);
+int set_pkg_maintainer(Package* self, char* maintainer);
+int set_pkg_multiple_maintainers(Package* self, char** maintainers);
 #endif /* ANCIENTPKG_H */
---
+++ src/ancientpkg/package.c
@@ -0,0 +1,65 @@
+#include "ancientpkg.h"
+#include <common/ansiescapes.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int validate_package(Package* self)
+{
+    int has_missing = 0;
+    if (self->maintainer == NULL)
+    {
+        printf(ERROR "Missing parameter \"maintainer\" in package.\n");
+        has_missing = 1;
+    }
+    if (!strcmp(self->maintainer, "multiple"))
+    {
+        if (self->maintainers_ls == NULL)
+        {
+            printf(ERROR "Missing parameter \"maintainers\" in package.\n");
+            has_missing = 1;
+        }
+    }
+    else
+    {
+        if (self->maintainers_ls != NULL)
+        {
+            printf(ERROR "Both \"maintainer\" and \"maintainers\" fields "
+                   "were filled in.\n");
+            has_missing = 1;
+        }
+    }
+    if (self->name == NULL)
+    {
+        printf(ERROR "Missing parameter \"packageName\" in package.\n");
+        has_missing = 1;
+    }
+    if (self->license == NULL)
+    {
+        printf(ERROR "Missing parameter \"license\" in package.\n");
+        has_missing = 1;
+    }
+
+    return has_missing;
+}
+
+int create_package(Package* self, char* name, char* license)
+{
+    self->name = name;
+    self->license = license;
+    return 0;
+}
+
+int set_pkg_maintainer(Package* self, char* maintainer)
+{
+    self->maintainer = maintainer;
+    return 0;
+}
+
+int set_pkg_multiple_maintainers(Package* self, char** maintainers)
+{
+    self->maintainer = "multiple";
+    self->maintainers_ls = maintainers;
+    return 0;
+}
