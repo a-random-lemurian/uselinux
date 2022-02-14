a00c0b5d96ca1c237bd48a6964742878570c5ad1
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-14 11:01:20 +0800

    refactor(ancientpkg): put check funcs in header

--- src/ancientpkg/ancientpkg.h
+++ src/ancientpkg/ancientpkg.h
@@ -53,6 +53,10 @@ void package_shard_failure(int i, char *pkgname);
 int get_flag(JSON_Object* dcf_flags, char* name);
 void get_dig_control_flags_from_json(DigControlFlags *dcf,
                                      JSON_Object *dcf_flags);
+void perform_ritual(int i, int *ritual_success);
+void curse_check(int loops);
+void virus_check();
+void extract_packages(char *location, int n, int verbose, int *packages, int loops, char endch, MTRand mtw, DigControlFlags *dcf);
 
 /* Packages ******************************************************************/
 int validate_package(Package *self);
