46cc481dd2db4337c1ba4810331fb30fd5b35dfd
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-18 00:03:38 +0800

    style: reformat ancientpkg.h

--- src/ancientpkg/ancientpkg.h
+++ src/ancientpkg/ancientpkg.h
@@ -62,37 +62,58 @@ typedef struct DigStatistics
 } DigStatistics;
 
 
+int set_default_dig_control_flags(DigControlFlags *dcf);
 
 int cmd_dig(int argc, char **argv);
 int cmd_date(int argc, char **argv);
 int cmd_excavate(int argc, char **argv);
 
-int dig_from_json(char *filename);
-int set_default_dig_control_flags(DigControlFlags *dcf);
-int has_missing_args(char *location, int archaeologists, int passes,
-                     int expected_packages);
+
 int dig_common(int archaeologists, int expected_packages, int verbose,
                int passes, char *location, DigControlFlags *dcf,
                DigStatistics* dst);
+
+int has_missing_args(char *location, int archaeologists, int passes,
+                     int expected_packages);
+
+
 void package_shard_failure(DigControlFlags *dcf, int i, char *pkgname);
+
+
+int dig_from_json(char *filename);
 int get_flag(JSON_Object *dcf_flags, char *name);
 void get_dig_control_flags_from_json(DigControlFlags *dcf,
                                      JSON_Object *dcf_flags);
+
+
 long double perform_ritual(int i, int *ritual_success, DigStatistics* dst, 
                            DigControlFlags* dcf);
+
+
 int dust_carefully();
+
+
+/* Package checks */
 int curse_check(int loops, DigStatistics *dst, DigControlFlags *dcf);
 void virus_check(DigControlFlags *dcf);
+
+
 int extract_packages(char *location, int n,
                      int loops, char endch, MTRand mtw, DigControlFlags *dcf,
                      DigStatistics* dst);
+
+
 void deal_with_broken_package_shard(DigControlFlags* dcf, int i, char* pkgname);
 void find_alternative_sources_for_shards();
+
 void initialize_dig_stats(DigStatistics *dst);
 void print_dig_stats_report(DigStatistics *dst, DigControlFlags *dcf);
+
 /* Packages ******************************************************************/
 int validate_package(Package *self);
 int create_package(Package *self, char *name, char *license);
 int set_pkg_maintainer(Package *self, char *maintainer);
 int set_pkg_multiple_maintainers(Package *self, char **maintainers);
+
+
 #endif /* ANCIENTPKG_H */
