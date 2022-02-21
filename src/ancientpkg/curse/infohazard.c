56b9f6feed83da57fccf54b57f4c22acc8c20c3c
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-22 00:06:28 +0800

    feat(ancientpkg): add infohazard mitigation system

--- src/ancientpkg/curse/infohazard.c
+++ src/ancientpkg/curse/infohazard.c
@@ -4,6 +4,23 @@
 #include <common/mtwister.h>
 #include <stdio.h>
 #include <stdint.h>
+#include "infohazard.h"
+
+static Infohazard infohazards[] = {
+    {10, "Berryman-Langford Memetic Kill Agent", ih_berryman_langford}
+};
+
+int random_infohazard(DigControlFlags *dcf, DigStatistics *dst)
+{
+    size_t ih_sz = sizeof(infohazards)/sizeof(infohazards[0]);
+    size_t idx = randint(1, ih_sz) - 1;
+
+    print_infohazard_message(&infohazards[idx]);
+    
+    Infohazard* detected_infohazard = &infohazards[idx];
+
+    detected_infohazard->fn(dcf, dst);
+}
 
 int cmit_infohazard(int n, DigControlFlags *dcf, DigStatistics *dst)
 {
@@ -14,17 +31,38 @@ int cmit_infohazard(int n, DigControlFlags *dcf, DigStatistics *dst)
 
     uint64_t bytes_suppressed = lvl * 50 * randint(20, 50) * 900 * (clock()/2);
 
-    for (int i = 0; i < randint(30, 140); i++)
+    if (!dcf->dry_run)
     {
-        if (!dcf->dry_run)
-        {
-            msleep(randint(40, 90));
-        }
-
+        msleep(randint(40, 90));
+    }
+    else
+    {
+        random_infohazard(dcf, dst);
     }
 
     printf("%lu bytes of infohazardous memory was suppressed.\n",
            bytes_suppressed);
 
     return 0;
+}
+
+int print_infohazard_message(Infohazard* ih)
+{
+    printf(WARN "Infohazard %s with danger level %d detected.\n",
+           ih->name, ih->danger);
+    return 0;
+}
+
+int ih_berryman_langford(DigControlFlags *dcf, DigStatistics *dst)
+{
+    int max = randint(7, 23);
+
+    for (int i = 0; i < max; i++)
+    {
+        printf(WARN "Expunging data (%d/%d)\n", i, max);
+        if (!dcf->dry_run)
+        {
+            msleep(randint(30, 50));
+        }
+    }
 }
---
+++ src/ancientpkg/curse/infohazard.h
@@ -0,0 +1,14 @@
+#ifndef INFOHAZARD_H
+#define INFOHAZARD_H
+#include <ancientpkg.h>
+
+typedef struct Infohazard {
+    int danger;
+    char *name;
+    int (*fn)(DigControlFlags *, DigStatistics *);
+} Infohazard;
+
+int ih_berryman_langford(DigControlFlags *dcf, DigStatistics *dst);
+int print_infohazard_message(Infohazard* ih);
+
+#endif /* INFOHAZARD_H */
