aeb9f326e6340604748e258d51830f5e4e29f226
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-02-28 14:07:31 +0800

    feat(ancientpkg): add hieroglyph system

--- src/ancientpkg/curse/ae-curse.c
+++ src/ancientpkg/curse/ae-curse.c
@@ -9,6 +9,99 @@
 #include <string.h>
 #include <strings.h>
 
+static int last_decode_attempt_was_error = 0;
+static int consecutive_decode_failures = 0;
+static int first_failed_hglyph = 0;
+
+int deal_with_single_hglyph_error(int i, int rc)
+{
+
+    char endch = '\r';
+
+    printf("[Decode] "BHRED"Error:"reset" Failed to decode hieroglyph %d-%d.",
+           first_failed_hglyph, i);
+
+    if (last_decode_attempt_was_error == 2)
+    {
+        first_failed_hglyph = i;
+        last_decode_attempt_was_error = 1;
+    }
+
+    if (last_decode_attempt_was_error == 1)
+    {
+        consecutive_decode_failures++;
+        printf(" (%d consecutive failures)", consecutive_decode_failures);
+    }
+    else
+    {
+        last_decode_attempt_was_error = 2;
+    }
+
+    printf("%c", endch);
+    fflush(stdout);
+
+    return 0;
+}
+
+int decode_single_hglyph(DigControlFlags* dcf, DigStatistics* dst)
+{
+    if (!dcf->dry_run)
+    {
+        msleep(randint(40, 400));
+    }
+
+
+    int n = randint(1, 120);
+
+    if (n > 115)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int remove_error_state()
+{
+    last_decode_attempt_was_error = 0;
+    consecutive_decode_failures = 0;
+
+    return 0;
+}
+
+int decode_hieroglyphs(DigControlFlags* dcf, DigStatistics* dst)
+{
+    int i = 0;
+    int rc = 0;
+    int hglyphs_required = randint(4, 90);
+    int hglyphs_decoded = 0;
+
+    while (hglyphs_decoded < hglyphs_required)
+    {
+        rc = decode_single_hglyph(dcf, dst);
+
+        if (rc != 0)
+        {
+            deal_with_single_hglyph_error(i, rc);
+        }
+        else if (rc == 0)
+        {
+            remove_error_state();
+
+            printf("\n[Decode] Decoded hieroglyph %d.\n", i);
+            hglyphs_decoded++;
+        }
+
+        i++;
+    }
+
+    printf("[Decon] Was able to derive curse mitigation method from "
+           "hieroglpyhs.\n");
+    printf("[Decon] Broke curse.\n");
+}
+
 int cmit_ancient_egypt_curse(int n, DigControlFlags *dcf, DigStatistics *dst)
 {
     if (strncasecmp(dcf->location, "Egypt", 6) != 0)
@@ -31,6 +124,14 @@ int cmit_ancient_egypt_curse(int n, DigControlFlags *dcf, DigStatistics *dst)
     for (int i = 0; i < k; i++)
     {
         printf("[Decon] Removing curse from package (step %d of %d).\n", i, k);
+
+        int l = randint(1, 900);
+
+        if (l > 850)
+        {
+            printf("[Decon] Warning: Package fragment contains sub-curse.\n");
+            decode_hieroglyphs(dcf, dst);
+        }
     }
 
     return 0;
