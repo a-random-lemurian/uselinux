673a13aa8fa27e991712e09aa21b83f81a01474d
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-11-19 04:06:00 +0800

    docs: document ancientpkg.h

--- src/ancientpkg/ancientpkg.h
+++ src/ancientpkg/ancientpkg.h
@@ -71,59 +71,218 @@ typedef struct PackageCurse
     int (*fn)(int, DigControlFlags*, DigStatistics*);
 } PackageCurse;
 
-
+/**
+ * @brief Set the default dig control flags in an object.
+ * 
+ * @param dcf The object.
+ * @return int An integer.
+ */
 int set_default_dig_control_flags(DigControlFlags *dcf);
 
+/**
+ * @brief The command line interface for the dig command.
+ */
 int cmd_dig(int argc, char **argv);
+
+/**
+ * @brief The command line interface for the date command.
+ */
 int cmd_date(int argc, char **argv);
+
+/**
+ * @brief The command line interface for the excavate command.
+ */
 int cmd_excavate(int argc, char **argv);
+
+/**
+ * @brief The command line interface for the install command.
+ */
 int cmd_install(int argc, char** argv);
 
 
+/**
+ * @brief Extract the packages.
+ * 
+ * @param archaeologists Number of archaeologists
+ * @param expected_packages Packages expected
+ * @param passes Number of passes to make over the site
+ * @param location Where to extract them
+ * @param dcf Dig control flags object
+ * @param dst Dig statistics object
+ * @return int An integer
+ */
 int dig_common(int archaeologists, int expected_packages,
                int passes, char *location, DigControlFlags *dcf,
                DigStatistics* dst);
 
+/**
+ * @brief Check for missing args
+ * 
+ * @param location Location argument
+ * @param archaeologists Archaeologists argument
+ * @param passes Passes argument
+ * @param expected_packages Expected packages argument
+ * @return int 
+ */
 int has_missing_args(char *location, int archaeologists, int passes,
                      int expected_packages);
 
 
 void package_shard_failure(DigControlFlags *dcf, int i, char *pkgname);
 
-
+/**
+ * @brief Launch a dig job with parameters from a JSON file
+ * 
+ * @param filename Name of JSON file
+ * @return int An integer
+ */
 int dig_from_json(char *filename);
+
+/**
+ * @brief Get a flag from a parson JSON object
+ * 
+ * @param dcf_flags The parson JSON object
+ * @param name The name of the flag
+ * @return int The integer result code of the flag
+ */
 int get_flag(JSON_Object *dcf_flags, char *name);
+
+/**
+ * @brief Get the dig control flags from a parson JSON object.
+ * 
+ * @param dcf The dig control flag object to modify
+ * @param dcf_flags The parson JSON object with the dig control flags.
+ */
 void get_dig_control_flags_from_json(DigControlFlags *dcf,
                                      JSON_Object *dcf_flags);
 
 
+/**
+ * @brief Unimplemented. Performs a ritual.
+ * 
+ * @param i Undocumented due to alien codebase.
+ * @param ritual_success An integer value that the function would set if the
+ * ritual worked.
+ *
+ * @param dst Dig statistics object
+ * @param dcf Dig contol flags
+ * @return long double Undocumented due to alien codebase.
+ */
 long double perform_ritual(int i, int *ritual_success, DigStatistics* dst, 
                            DigControlFlags* dcf);
 
 
+/**
+ * @brief Dust a package carefully
+ * 
+ * @return int The time spent dusting carefully
+ */
 int dust_carefully();
 
 
 /* Package checks */
+/**
+ * @brief Check for curses in the packages.
+ * 
+ * @param loops Loops to make.
+ * @param dst Dig statistics object
+ * @param dcf Dig control flags object
+ * @return int 
+ */
 int curse_check(int loops, DigStatistics *dst, DigControlFlags *dcf);
+
+/**
+ * @brief Check for viruses in the packages.
+ * 
+ * @param dcf Dig control flags object
+ */
 void virus_check(DigControlFlags *dcf);
 
-
+/**
+ * @brief Extract packages
+ * 
+ * @param location Where to extract
+ * @param n Undocumented due to alien codebase.
+ * @param loops Loops to make
+ * @param endch Newline character
+ * @param mtw Mersenne twister object
+ * @param dcf Dig control flags object
+ * @param dst Dig statistics object
+ * @return int 
+ */
 int extract_packages(char *location, int n,
                      int loops, char endch, MTRand mtw, DigControlFlags *dcf,
                      DigStatistics* dst);
 
-
+/**
+ * @brief Deal with a broken package shard.
+ * 
+ * @param dcf Object with flags to control the dig job.
+ * @param i Shard ID
+ * @param pkgname The package with the missing shard.
+ */
 void deal_with_broken_package_shard(DigControlFlags* dcf, int i, char* pkgname);
+
+/**
+ * @brief Find a shard, getting it from other sources
+ */
 void find_alternative_sources_for_shards();
 
+/**
+ * @brief Initialize a dig statistics object to its default values.
+ * 
+ * @param dst 
+ */
 void initialize_dig_stats(DigStatistics *dst);
+
+/**
+ * @brief Print a report of statistics during the dig.
+ * 
+ * @param dst Dig statistics object.
+ * @param dcf Object with flags to control the dig job.
+ */
 void print_dig_stats_report(DigStatistics *dst, DigControlFlags *dcf);
 
 /* Packages ******************************************************************/
+/**
+ * @brief Validate packages for common errors, at the moment it only checks
+ * for missing parameters.
+ * 
+ * @param self The package to validate
+ * @return int Whether there are missing parameters
+ */
 int validate_package(Package *self);
+
+/**
+ * @brief Create a package
+ * 
+ * @param self Package object
+ * @param name Name of package
+ * @param license License of package
+ * @return int
+ */
 int create_package(Package *self, char *name, char *license);
+
+/**
+ * @brief If a package has one maintainer, use this function to set the
+ * package's maintainer. For a package with multiple maintainers use
+ * the set_pkg_multiple_maintainers function instead.
+ * 
+ * @param self 
+ * @param maintainer 
+ * @return int 
+ */
 int set_pkg_maintainer(Package *self, char *maintainer);
+
+/**
+ * @brief If a package has a group of maintainers, use this function to
+ * set the package's maintainers. For a package with one maintainer use the
+ * set_pkg_maintainer function instead.
+ * 
+ * @param self The package to modify
+ * @param maintainers The maintainers of the package
+ * @return int Result code
+ */
 int set_pkg_multiple_maintainers(Package *self, char **maintainers);
 
 
