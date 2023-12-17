715976c3fb4489ba84ec1dd87868b667c4c48229
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2023-12-17 19:09:24 +0800

    Move has_missing_args(), digcommon.c -> dig.c

--- src/ancientpkg/dig.c
+++ src/ancientpkg/dig.c
@@ -9,6 +9,59 @@
 #include "ancientpkg.h"
 #include "ancientpkg_utils.h"
 
+/*
+ * Check the archaeologists, passes, location, and expected_packages variables
+ * and ensure that they are valid.
+ */
+static int has_missing_args(DigControlFlags *dcf, char *location,
+                            int archaeologists, int passes,
+                            int expected_packages)
+{
+    int had_fatal_err = 0;
+    if (location == NULL)
+    {
+        printf(ERROR "location must be specified\n");
+        had_fatal_err = 1;
+    }
+
+    if (!dcf->force_archaeologists)
+    {
+        if (archaeologists <= 1)
+        {
+            printf(ERROR "need more than 1 archaeologist.\n");
+            had_fatal_err = 1;
+        }
+
+        if (archaeologists > 8000000000)
+        {
+            printf(ERROR
+                   "Too many archaeologists (not everyone in the world is "
+                   "one,)\n");
+            had_fatal_err = 1;
+        }
+    }
+
+    if (passes <= 0)
+    {
+        printf(ERROR "need more than 1 pass.\n");
+        had_fatal_err = 1;
+    }
+
+    if (expected_packages <= 0)
+    {
+        printf(ERROR "need to expect more than 1 package.\n");
+        had_fatal_err = 1;
+    }
+
+    if (had_fatal_err)
+    {
+        printf("use --help for help.\n");
+        return 1;
+    }
+
+    return had_fatal_err;
+}
+
 int cmd_dig(int argc, char **argv)
 {
     int archaeologists = -1;
--- src/ancientpkg/digcommon.c
+++ src/ancientpkg/digcommon.c
@@ -265,58 +265,6 @@ int dig_common(int archaeologists, int expected_packages, int passes,
 }
 
 /*
- * Check the archaeologists, passes, location, and expected_packages variables
- * and ensure that they are valid.
- */
-int has_missing_args(DigControlFlags *dcf, char *location, int archaeologists,
-                     int passes, int expected_packages)
-{
-    int had_fatal_err = 0;
-    if (location == NULL)
-    {
-        printf(ERROR "location must be specified\n");
-        had_fatal_err = 1;
-    }
-
-    if (!dcf->force_archaeologists)
-    {
-        if (archaeologists <= 1)
-        {
-            printf(ERROR "need more than 1 archaeologist.\n");
-            had_fatal_err = 1;
-        }
-
-        if (archaeologists > 8000000000)
-        {
-            printf(ERROR
-                   "Too many archaeologists (not everyone in the world is "
-                   "one,)\n");
-            had_fatal_err = 1;
-        }
-    }
-
-    if (passes <= 0)
-    {
-        printf(ERROR "need more than 1 pass.\n");
-        had_fatal_err = 1;
-    }
-
-    if (expected_packages <= 0)
-    {
-        printf(ERROR "need to expect more than 1 package.\n");
-        had_fatal_err = 1;
-    }
-
-    if (had_fatal_err)
-    {
-        printf("use --help for help.\n");
-        return 1;
-    }
-
-    return had_fatal_err;
-}
-
-/*
  * Set the dig control flags to their default values. Always call this method
  * when preparing a DigControlFlags struct to prevent undefined values and
  * behavior.
